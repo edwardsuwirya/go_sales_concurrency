94b0cc97308a1ade93f9ab5729a7e865ac50c598
Edward Suwirya <edward_suwirya@yahoo.com>
2022-05-18 14:44:35 +0700

    Modular function channel

--- app.go
+++ app.go
@@ -18,21 +18,78 @@ type salesData struct {
 	sales      int
 }
 
-func salesCaluction(wg *sync.WaitGroup, fileName string) {
-	jobs := make(chan salesData)
-	file, err := os.Open(fileName)
+var workDir = "/Users/edwardsuwirya/Desktop/sample_data"
+
+func salesDataFileList() []fs.FileInfo {
+	files, err := ioutil.ReadDir(workDir)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+func readSalesData(fileName string) []salesData {
+	file, err := os.Open(filepath.Join(workDir, fileName))
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	go func() {
-		var totalSales int
-		for s := range jobs {
-			totalSales = totalSales + s.sales
+	scanner := bufio.NewScanner(file)
+	var listSales []salesData
+	for scanner.Scan() {
+		str := scanner.Text()
+		split := strings.Split(str, ",")
+		outletCode := split[0]
+		sales, _ := strconv.Atoi(split[1])
+		data := salesData{
+			outletCode,
+			sales,
 		}
-		fmt.Println(fileName, totalSales)
-		wg.Done()
-	}()
+		listSales = append(listSales, data)
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return listSales
+}
+
+func calculateSales(fileName string) int {
+	var totalSales int
+	for _, s := range readSalesData(fileName) {
+		totalSales = totalSales + s.sales
+	}
+	return totalSales
+}
+
+func channelCalculation() {
+	var wg sync.WaitGroup
+	for _, f := range salesDataFileList() {
+		wg.Add(1)
+		go func(f fs.FileInfo) {
+			readSalesDataChannel(&wg, f.Name())
+			//fmt.Println(f.Name(), totalSales)
+		}(f)
+	}
+	wg.Wait()
+}
+
+func calculateSalesChannel(wg *sync.WaitGroup, jobs chan salesData) {
+	var totalSales int
+	for s := range jobs {
+		totalSales = totalSales + s.sales
+	}
+	//fmt.Println(fileName, totalSales)
+	wg.Done()
+}
+func readSalesDataChannel(wg *sync.WaitGroup, fileName string) {
+	jobs := make(chan salesData)
+	file, err := os.Open(filepath.Join(workDir, fileName))
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	go calculateSalesChannel(wg, jobs)
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		str := scanner.Text()
@@ -50,25 +107,10 @@ func salesCaluction(wg *sync.WaitGroup, fileName string) {
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
-
 }
 func main() {
 	startTime := time.Now()
-	workDir := "/Users/edwardsuwirya/Desktop/sample_data"
-	files, err := ioutil.ReadDir(workDir)
-	if err != nil {
-		panic(err)
-	}
-
-	var wg sync.WaitGroup
-	for _, f := range files {
-		wg.Add(1)
-		go func(f fs.FileInfo) {
-			salesCaluction(&wg, filepath.Join(workDir, f.Name()))
-		}(f)
-	}
-	wg.Wait()
-
+	channelCalculation()
 	diff := time.Now().Sub(startTime)
 	fmt.Printf("Took: %f seconds\n", diff.Seconds())
 
